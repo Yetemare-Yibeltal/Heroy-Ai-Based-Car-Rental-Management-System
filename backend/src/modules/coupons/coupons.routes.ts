b79eeb4c61d5b5b144c9ebaf81d5997f3f858e46
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-07-10 03:20:21 +0300

    add coupons routes

---
+++ backend/src/modules/coupons/coupons.routes.ts
@@ -0,0 +1,48 @@
+import { Router } from 'express';
+import * as couponsController from './coupons.controller';
+import { validate } from '../../middleware/validate';
+import { authenticate, authorize } from '../../middleware/auth';
+import { catchAsync } from '../../utils/catchAsync';
+import { ADMIN_ROLES } from '../../constants/enums';
+import { createCouponSchema, updateCouponSchema } from './coupons.validation';
+
+export const couponsRoutes = Router();
+
+couponsRoutes.get('/check/:code', catchAsync(couponsController.checkCoupon));
+
+couponsRoutes.get(
+  '/',
+  authenticate,
+  authorize(...ADMIN_ROLES),
+  catchAsync(couponsController.listCoupons)
+);
+
+couponsRoutes.post(
+  '/',
+  authenticate,
+  authorize(...ADMIN_ROLES),
+  validate(createCouponSchema),
+  catchAsync(couponsController.createCoupon)
+);
+
+couponsRoutes.patch(
+  '/:id',
+  authenticate,
+  authorize(...ADMIN_ROLES),
+  validate(updateCouponSchema),
+  catchAsync(couponsController.updateCoupon)
+);
+
+couponsRoutes.patch(
+  '/:id/deactivate',
+  authenticate,
+  authorize(...ADMIN_ROLES),
+  catchAsync(couponsController.deactivateCoupon)
+);
+
+couponsRoutes.delete(
+  '/:id',
+  authenticate,
+  authorize(...ADMIN_ROLES),
+  catchAsync(couponsController.deleteCoupon)
+);
